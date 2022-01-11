04d7f9ceab6f7561fb3113f3751f69cc2e9d6c17
Grigoriy Lizanets <grigoriylizanets@gmail.com>
2022-01-12 01:18:20 +0200

    Fixed Two Components

---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+
+
+const Products = () => {
+    return(
+        <div>
+            <h1>The Products Page</h1>
+        </div>
+    )
+}
+
+export default Products
---
+++ src/components/Wellcome.js
@@ -0,0 +1,11 @@
+
+
+const Wellcome = () => {
+    return(
+        <div>
+            <h1>The Wellcome Page</h1>
+        </div>
+    )
+}
+
+export default Wellcome
