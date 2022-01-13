a56de273274a836d63d664745213a20cee83ac1a
Grigoriy Lizanets <grigoriylizanets@gmail.com>
2022-01-13 16:18:27 +0200

    A little changes, but from a new Folder

--- src/pages/Wellcome.js
+++ src/pages/Wellcome.js
@@ -4,6 +4,7 @@ const Wellcome = () => {
     return(
         <section>
             <h1>The Wellcome Page</h1>
+            <p>And some changes in this page</p>
             <Route path='/wellcome/new-user'>
                 <p>Wellcome, new User!</p>
             </Route>
